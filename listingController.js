cf4b8db7699414e87bc0b5c94d03129f3b0a2d76
iTwicKz <twickes32@ufl.edu>
2016-01-13 17:49:23 -0500

    Add functions for JSON

--- listingController.js
+++ listingController.js
@@ -3,13 +3,40 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
+
     /*
       Implement these functions in the controller to make your application function
       as described in the assignment spec.
      */
-     
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
+
+    $scope.addListing = function(user) {
+      // $scope.user = angular.copy($scope.master);
+
+      // var fs = require('fs');
+
+      // $scope.listings = $scope.listings.concat
+      taker =       {
+                "code": "AAF",
+                "name": "Academic Advisement - Farrior Hall",
+                "coordinates": {
+                    "latitude": 29.6502323,
+                    "longitude": -82.34563860000002
+                },
+                "address": "100 Fletcher Dr, Gainesville, FL 32611, United States"
+            }
+
+      console.log("YEAH!!")
+      console.log(user)
+      $scope.listings.push(user)
+
+      // JSON.stringify($scope.listings);
+      // fs.writeFile('listingFactory.json', JSON.stringify($scope.listings));
+
+
+    };
+    $scope.deleteListing = function(index) {
+
+    };
     $scope.showDetails = function(index) {};
   }
 ]);
