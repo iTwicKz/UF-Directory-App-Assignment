f8a71aa6b77b87809b92cd787019c15f9139f0f3
iTwicKz <twickes32@ufl.edu>
2016-01-15 23:58:40 -0500

    Added animation, merged

--- listingController.js
+++ listingController.js
@@ -2,41 +2,31 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
-
-    /*
-      Implement these functions in the controller to make your application function
-      as described in the assignment spec.
-     */
-
-    $scope.addListing = function(user) {
-      // $scope.user = angular.copy($scope.master);
-
-      // var fs = require('fs');
-
-      // $scope.listings = $scope.listings.concat
-      taker =       {
-                "code": "AAF",
-                "name": "Academic Advisement - Farrior Hall",
-                "coordinates": {
-                    "latitude": 29.6502323,
-                    "longitude": -82.34563860000002
-                },
-                "address": "100 Fletcher Dr, Gainesville, FL 32611, United States"
-            }
-
-      console.log("YEAH!!")
-      console.log(user)
-      $scope.listings.push(user)
-
-      // JSON.stringify($scope.listings);
-      // fs.writeFile('listingFactory.json', JSON.stringify($scope.listings));
-
-
+    $scope.newListing = {
+      coordinates: {}
     };
+
+    $scope.addListing = function() {
+      var newListingToInsert = {
+        name: $scope.newListing.name,
+        code: $scope.newListing.code,
+        address: $scope.newListing.address,
+        coordinates: {
+          latitude: $scope.newListing.coordinates.latitude,
+          longitude: $scope.newListing.coordinates.longitude
+        }
+      }
+      $scope.listings.push(newListingToInsert);
+      clearForm();
+    };
+
     $scope.deleteListing = function(index) {
-      $scope.listings.splice(index, 1)
+      $scope.listings.splice(index, 1);
     };
-    $scope.showDetails = function(index) {};
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+      $scope.detailedInfo.showDetails = true
+    };
   }
 ]);
